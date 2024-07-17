c097e56d1b3666d779d842980ce4d74009a7241b
htetyadanar-hyo <htetyadanar.hyo@gmail.com>
2024-07-17 10:16:52 -0500

    Removed .idea directory

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,1 +1,36 @@
-export default function Event(){}
+import React, { useState } from 'react';
+import CreateEvent from './CreateEvent';
+import AllEvents from './AllEvents';
+import '../styles/Event.css';
+
+const Event = () => {
+    const [view, setView] = useState('');
+    const userRole = localStorage.getItem('userRole');
+
+    const handleViewChange = (view) => {
+        setView(view);
+    };
+
+    return (
+        <div className="event-container">
+            {userRole === 'ADMIN' ? (
+                <>
+                    <div className="button-container">
+                        <button onClick={() => handleViewChange('create')} className="view-button">Create Event</button>
+                        <br/><br/>
+                        <button onClick={() => handleViewChange('all')} className="view-button">All Events</button>
+                    </div>
+                    <br /><br/>
+                    <div className="view-container">
+                        {view === 'create' && <CreateEvent />}
+                        {view === 'all' && <AllEvents />}
+                    </div>
+                </>
+            ) : (
+                <div className="blank-container"></div>
+            )}
+        </div>
+    );
+};
+
+export default Event;
